0b8c70102c2c9042f16b51fad0258e05f6eedc57
Deepak-Verma-556 <deepakkhudia46015@gmail.com>
2024-10-14 10:37:45 +0530

    add tailwind

---
+++ src/components/SimpleSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SimpleSlider = () => {
+  return (
+
+  )
+}
+
+export default SimpleSlider
