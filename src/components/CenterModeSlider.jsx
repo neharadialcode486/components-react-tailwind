c79d7b4cdb4731469f1e1e4e4be6069f51c972bc
Deepak-Verma-556 <deepakkhudia46015@gmail.com>
2024-10-14 13:53:37 +0530

    download images

---
+++ src/components/CenterModeSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+export default function CenterModeSlider(){
+
+};
+return (
+    
+)
