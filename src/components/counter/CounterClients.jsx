b2980fc7f09348d67668d86f933a1c2fca915700
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-10-15 14:24:14 +0530

    image convert png to webp

--- src/components/counter/CounterClients.jsx
+++ src/components/counter/CounterClients.jsx
@@ -13,10 +13,13 @@ const CounterClients = () => {
                     if (entry.isIntersecting && !hasAnimated) {
                         CLIENTS_DATA.forEach((item, index) => {
                             let start = 0;
-                            const end = parseInt(item.number);
+                            const isProfit = item.text === "GAINED IN PROFIT";
+                            const end = parseInt(item.number.replace(/[^0-9]/g, ''));
+                            const intermediateValue = 50000;
+                            const threshold = isProfit ? 50 : Math.floor(end * 0.8);
                             const increment = 1;
                             const duration = 2000;
-                            const stepTime = Math.abs(Math.floor(duration / (end - start)));
+                            const stepTime = Math.abs(Math.floor(duration / (threshold - start)));
                             const timer = setInterval(() => {
                                 start += increment;
                                 setPercentages((prevPercentages) => {
@@ -24,13 +27,27 @@ const CounterClients = () => {
                                     newPercentages[index] = start;
                                     return newPercentages;
                                 });
-
-                                if (start >= end) {
+                                if (start >= threshold) {
                                     clearInterval(timer);
+                                    if (isProfit) {
+                                        setTimeout(() => {
+                                            setPercentages((prevPercentages) => {
+                                                const newPercentages = [...prevPercentages];
+                                                newPercentages[index] = intermediateValue;
+                                                return newPercentages;
+                                            });
+                                            setTimeout(() => {
+                                                setPercentages((prevPercentages) => {
+                                                    const newPercentages = [...prevPercentages];
+                                                    newPercentages[index] = end;
+                                                    return newPercentages;
+                                                });
+                                            }, 500);
+                                        }, 300);
+                                    }
                                 }
                             }, stepTime);
                         });
-
                         setHasAnimated(true);
                     }
                 });
@@ -50,16 +67,19 @@ const CounterClients = () => {
     return (
         <div ref={sectionRef} className="lg:py-24 sm:py-20 py-16 relative">
             <div className="container relative z-10">
-                <h2 className='text-center md:text-4xl max-w-[662px] w-full mx-auto text-3xl lg:text-[40px] font-bold'>Clients trust us with their <span className='text-medium-orange'>Amazon strategy for a reason.</span></h2>
-                <div className="lg:mt-14 sm:mt-8 pt-3 md:pt-1 max-w-[1086px] w-full flex flex-row flex-wrap md:flex-nowrap justify-center md:justify-between mx-auto md:gap-4">
+                <h2 className='text-center md:text-4xl max-w-[662px] w-full mx-auto text-3xl lg:text-[40px] font-bold'>
+                    Clients trust us with their <span className='text-medium-orange'>Amazon strategy for a reason.</span>
+                </h2>
+                <div className="lg:mt-14 pt-3 md:pt-1 max-w-[1086px] w-full flex flex-row flex-wrap lg:flex-nowrap justify-center lg:justify-between mx-auto lg:gap-4">
                     {CLIENTS_DATA.map((item, idx) => (
-                        <div key={idx} className={`flex px-3 mt-6 md:mt-0 w-full md:px-0 ${idx === 0 ? "justify-start" : idx === 1 ? "justify-center" : "justify-end"}`}>
-                            <div className='max-w-[308px] border border-lighter-orange duration-300 ease-linear hover:border-medium-orange bg-white rounded-md min-h-[140px] gap-5 p-3 relative w-full flex items-center justify-center'>
+                        <div key={idx} className={`flex px-3 mt-6 lg:mt-0 w-full md:w-1/2 lg:w-full lg:px-0 ${idx === 0 ? "lg:max-w-[308px]" : idx === 1 ? "lg:max-w-[322px]" : "lg:max-w-[356px]"}`}>
+                            <div className={`border border-lighter-orange duration-300 ease-linear hover:border-medium-orange bg-white rounded-md min-h-[140px] gap-5 p-5 relative w-full flex items-center justify-center `}>
                                 <img src={item.img} alt="icon" className='max-w-[70px] w-full' />
                                 <div className="flex flex-col">
                                     <p className='font-inter text-[40px] font-bold'>
-                                        {percentages[idx]}
+                                        {idx === 2 ? "$" : ""}{percentages[idx]}{idx === 0 || idx === 2 ? "+" : ""}
                                     </p>
+                                    <p className='mt-1 uppercase font-bold text-base'>{item.text}</p>
                                 </div>
                             </div>
                         </div>
--- src/utils/Helper.jsx
+++ src/utils/Helper.jsx
@@ -1,13 +1,14 @@
-import brands from "../assets/images/webp/brands.png";
-import languages from "../assets/images/webp/languages.png";
-import profit from "../assets/images/webp/profit.png";
+import brands from "../assets/images/webp/brands.webp";
+import languages from "../assets/images/webp/languages.webp";
+import profit from "../assets/images/webp/profit.webp";
+
 export const PLATFORM_DATA = [
     { number: "50%", text: "Reduction in Pain" },
     { number: "90%", text: "Increase In-Patient Satisfaction" },
     { number: "60%", text: "Faster Recovery" },
-]
+];
 export const CLIENTS_DATA = [
-    { img: brands, number: "20%", text: "BRANDS MANAGED" },
-    { img: languages, number: "07%", text: "Increase In-Patient Satisfaction" },
-    { img: profit, number: "100%", text: "Faster Recovery" },
-]
+    { img: brands, number: "20", text: "BRANDS MANAGED" },
+    { img: languages, number: "07", text: "LANGUAGES SPOKEN" },
+    { img: profit, number: "$100,000", text: "GAINED IN PROFIT" },
+];
