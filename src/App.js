e00052a7a376e22db967c693d1dc8539e91b76c9
ritu-477 <ritujoharbishnoi@gmail.com>
2024-10-16 22:53:44 +0530

    done-third-roadmap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import RoadMapFirst from './components/RoadMapFirst';
 import RoadMapSecond from './components/RoadMapSecond';
+import RoadMapThird from './components/RoadMapThird';
 
 function App() {
   return (
     <>
       <RoadMapFirst />
-      <RoadMapSecond/>
+      <RoadMapSecond />
+      <RoadMapThird />
     </>
   );
 }
--- src/utils/Helper.jsx
+++ src/utils/Helper.jsx
@@ -6,4 +6,4 @@ export const ROADMAP_Items = [
     { title: 'Lorem ipsum 4', description: 'Lorem ipsum dolor sit amet consectetur.' },
     { title: 'Lorem ipsum 5', description: 'Lorem ipsum dolor sit amet consectetur.' },
 ];
-export default ROADMAP_Items;
+
