ac654042f12128dde27072a3b0118048c4f8e0f3
tusharswami534 <tusharswami17@gmail.com>
2024-10-17 15:18:10 +0530

    complete KeyframeAnimationSlider

--- src/components/KeyframeAnimationSlider.jsx
+++ src/components/KeyframeAnimationSlider.jsx
@@ -10,40 +10,40 @@ const KeyframeAnimationSlider = () => {
         <div className='bg-black min-h-screen flex items-center'>
             <div className='flex overflow-hidden'>
                 <div className='slide-scroll flex'>
-                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
-                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
-                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
-                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
-                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
-                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                        <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                        <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                        <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                        <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                        <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                        <img src={iconFifthImg} alt="icon-5" className='icons-size' />
                 </div>
             </div>
         </div>
