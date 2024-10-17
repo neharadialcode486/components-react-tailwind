b037588c692e5ed5e4e49db62b59b47dbc6603ce
tusharswami534 <tusharswami17@gmail.com>
2024-10-17 15:04:49 +0530

    Keyframe slider complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-import HeroFirst from './components/HeroFirst';
-import HeroSecond from './components/HeroSecond';
-import HeroThird from './components/HeroThird';
-import HeroFourth from './components/HeroFourth';
-import HeroFifth from './components/HeroFifth';
-import HeroSixth from './components/HeroSixth';
-import HeroSeventh from './components/HeroSeventh';
-import HeroEighth from './components/HeroEighth';
-import HeroNinth from './components/HeroNinth';
+import KeyframeAnimationSlider from './components/KeyframeAnimationSlider'
+
 
 function App() {
   return (
    <>
-   <HeroFirst/>
-    <HeroSecond/>
-   <HeroThird/>
-   <HeroFourth/>
-   <HeroFifth/>
-   <HeroSixth/>
-   <HeroSeventh/>
-   <HeroEighth/>
-   <HeroNinth/>
+   <KeyframeAnimationSlider/>
    </>
   );
 }
---
+++ src/components/KeyframeAnimationSlider.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import iconFirstImg from '../assets/images/png/icon-1.png'
+import iconSecondImg from '../assets/images/png/icon-2.png'
+import iconThirdImg from '../assets/images/png/icon-3.png'
+import iconFourthImg from '../assets/images/png/icon-4.png'
+import iconFifthImg from '../assets/images/png/icon-5.png'
+const KeyframeAnimationSlider = () => {
+  return (
+    <section>
+        <div className='bg-black min-h-screen flex items-center'>
+            <div className='flex overflow-hidden'>
+                <div className='slide-scroll flex'>
+                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                    <img src={iconFirstImg} alt="icon-1" className='icons-size' />
+                    <img src={iconSecondImg} alt="icon-2" className='icons-size' />
+                    <img src={iconThirdImg} alt="icon-3" className='icons-size' />
+                    <img src={iconFourthImg} alt="icon-4" className='icons-size' />
+                    <img src={iconFifthImg} alt="icon-5" className='icons-size' />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default KeyframeAnimationSlider
