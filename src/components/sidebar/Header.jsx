6b95e91c824c317361e246080d6d7c97afe678a7
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-11-08 12:31:53 +0530

    sidebar update

--- src/components/sidebar/Header.jsx
+++ src/components/sidebar/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ activeTab, setActiveTab, open, setOpen }) => {
 
     useEffect(() => {
         const savedTab = localStorage.getItem("activeTab");
-        const initialTab = savedTab ? parseInt(savedTab) : 1;
+        const initialTab = savedTab ? parseInt(savedTab) : 0;
         setActiveTab(initialTab);
     }, [setActiveTab]);
     useEffect(() => {
