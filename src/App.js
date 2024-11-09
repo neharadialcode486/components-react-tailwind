406a007f1b730b75149aebcd8a97ff00ed4097d8
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-11-09 09:55:13 +0530

    email js update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import SideBar from './components/sidebar/SideBar';
 import './App.css';
 
 function App() {
   return (
     <div className='max-w-[1920px] mx-auto relative'>
-      <SideBar />
+
     </div>
   );
 }
